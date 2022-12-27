59ca81fc25439b60a26deee89fcc4fe36bf99577
eyupGuc <meydanguresi@gmail.com>
2022-12-27 15:05:29 +0300

    interface

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,7 +1,13 @@
 import React from "react";
 import TodoListItem from "./TodoListItem";
 
+
+interface ITodoList {
+  todos: TodoType[]
+}
+
 const TodoList = () => {
+
   return (
     <ul>
       <TodoListItem />
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,18 +4,14 @@ import React, { useState } from "react";
 import InputForm from "../components/InputForm";
 import TodoList from "../components/TodoList";
 const url = "https://63aad50bfdc006ba604d4b4a.mockapi.io/todos";
-interface TodoType {
-  id: string | number;
-  task: string;
-  isDone: Boolean;
-}
+
 
 const Home = () => {
   const [todos, setTodos] = useState<TodoType[]>([]);
 
   const getTodos = async () => {
     try {
-      const { data } = await axios.get(url);
+      const { data } = await axios.get<TodoType[]>(url);
       console.log(data);
     } catch (e) {
       console.log(e);
